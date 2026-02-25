4f5bdb8bba7d84a2fe6f53b66490f0c9f1799fce
Roger Padrell <padrell.roger@gmail.com>
2026-02-25 18:45:07 +0100

    fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,6 @@
 if status is-interactive
 # Commands to run in interactive sessions can go here
 end
+
+oh-my-posh init fish --config .config/ohmyposh/zen.toml | source
+zoxide init fish --cmd cd | source
