bef419ee3d76e5ed8290375d506cd867aee2d252
Roger Padrell <padrell.roger@gmail.com>
2026-02-27 22:41:15 +0100

    ignore + update.sh

--- scripts/debloat.sh
+++ scripts/debloat.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 sudo pacman --noconfirm -Rns snapper limine-snapper-sync 
 sudo pacman --noconfirm -Rns chromium electron39 obsidian signal-desktop spotify typora localsend claude-code 1password-beta 1password-cli
 sudo pacman --noconfirm -Rns kirigami kwallet kio kdenlive qqc2-desktop-style solid sonnet syndication purpose
@@ -8,3 +9,4 @@ sudo rm -rf .local/share/applications/*
 
 sudo pacman -Rns --noconfirm libreoffice-fresh pinta obs-studio xournalpp neovim omarchy-nvim lazygit opencode starship gnome-calculator evince sushi 
 sudo omarchy-remove-preinstalls
+echo "Omarchy setup debloated"
---
+++ scripts/update.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+stow . --ignore .stow-local-ignore
+echo "Updated config from dotfiles"
