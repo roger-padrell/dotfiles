6e70c03f63c1bff4df48c811df285399b7718525
Roger Padrell <padrell.roger@gmail.com>
2026-02-27 23:15:32 +0100

    .

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -5,5 +5,7 @@ sleep 5
 echo "Installing dependencies..."
 yay -S --noconfirm yazi zen-browser-bin visual-studio-code-bin oh-my-posh stow ox fish
 
+echo "Setting theme..."
 mkdir ../.config/omarchy/themes/dark
 tar -xJf assets/theme.tar.xz -C ../.config/omarchy/themes/dark --strip-components=1
+omarchy-theme-set dark
