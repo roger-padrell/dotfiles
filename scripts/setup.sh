2510f36a8889e7c0a91a179acbe9cc553e7cb75b
Roger Padrell <padrell.roger@gmail.com>
2026-02-27 23:13:59 +0100

    Theming

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,4 +2,8 @@
 echo "Starting setup (5 seconds to cancel)..."
 sleep 5
 
-yay -S --noconfirm yazi zen-browser-bin code oh-my-posh stow ox
+echo "Installing dependencies..."
+yay -S --noconfirm yazi zen-browser-bin visual-studio-code-bin oh-my-posh stow ox fish
+
+mkdir ../.config/omarchy/themes/dark
+tar -xJf assets/theme.tar.xz -C ../.config/omarchy/themes/dark --strip-components=1
