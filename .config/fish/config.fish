d7f3627292b6f15712dc6dbdf19c9805c2e44611
Roger Padrell <padrell.roger@gmail.com>
2026-03-07 19:50:14 +0100

    flutter if exists

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,3 +4,12 @@ end
 
 oh-my-posh init fish --config .config/ohmyposh/zen.toml | source
 zoxide init fish --cmd cd | source
+
+# android
+set ANDROID_HOME "$HOME/Android/sdk"
+set ANDROID_SDK_ROOT "$HOME/Android/sdk"
+fish_add_path "$ANDROID_HOME/cmdline-tools/latest/bin"
+fish_add_path "$ANDROID_HOME/platform-tools"
+
+# flutter
+fish_add_path "$HOME/.flutter/flutter/bin"
