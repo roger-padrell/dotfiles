296d8e1c6c41df072cbadc689ec6c610902c9a1e
Roger Padrell <padrell.roger@gmail.com>
2026-03-14 08:27:15 +0100

    Setup

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,13 +3,9 @@ echo "Starting setup (5 seconds to cancel)..."
 sleep 5
 
 echo "Installing dependencies..."
-yay -S --noconfirm yazi zen-browser-bin visual-studio-code-bin oh-my-posh ox fish
-chsh -s /usr/bin/fish
-sudo chsh -s /usr/bin/fish
-
-echo "Setting theme..."
-mkdir ../.config/omarchy/themes/dark
-tar -xJf assets/theme.tar.xz -C ../.config/omarchy/themes/dark --strip-components=1
-omarchy-theme-set dark
+# TODO: install dependencies
+yay -S --noconfirm yazi zen-browser-bin visual-studio-code-bin oh-my-posh ox zsh
+chsh -s /usr/bin/zsh
+sudo chsh -s /usr/bin/zsh
 
 echo "All finished! You may need to reboot to finish some config"
--- scripts/update.sh
+++ scripts/update.sh
@@ -1,4 +1,12 @@
-#!/bin/bash
-sudo pacman --noconfirm -S stow
+#!/usr/bin/env sh
+
+# Check if stow is installed
+if pacman -Q stow >/dev/null 2>&1; then
+    echo "stow is already installed."
+else
+    echo "stow is not installed. Installing..."
+    sudo pacman -S --needed --noconfirm stow
+fi
+
 stow . --ignore .stow-local-ignore
 echo "Updated config from dotfiles"
